e962fecc99ef1e386807864390d8d32140ed10bf
hbgl <gohbgl@gmail.com>
2023-03-24 22:19:21 +0100

    Fix synchronous pkill on Linux.

--- src/test/support/process.ts
+++ src/test/support/process.ts
@@ -1,4 +1,6 @@
 import { sleepMs, sleepMsSync } from '../../support/sleep.js';
+import os from 'node:os';
+import fs from 'node:fs';
 
 export function procExists(pid: number) {
   try {
@@ -9,8 +11,31 @@ export function procExists(pid: number) {
   }
 }
 
+export function procAlive(pid: number) {
+  if (os.platform() === 'win32') {
+    return procExists(pid);
+  }
+  let statusRaw: string;
+  try {
+    statusRaw = fs.readFileSync(`/proc/${pid}/status`, 'utf-8');
+  } catch (e) {
+    const code = (e as any).code;
+    if (code === 'ENOENT') {
+      return false;
+    }
+    throw e;
+  }
+
+  const match = statusRaw.match(/^State:\s+(.)/m);
+  if (match === null) {
+    throw new Error('Invalid process status file: process state property missing.');
+  }
+  const status = match[1].toUpperCase();
+  return status !== 'Z' && status !== 'X';
+}
+
 export function killProcSync(pid: number, signal?: string | number) {
-  while (procExists(pid)) {
+  while (procAlive(pid)) {
     try {
       global.process.kill(pid, signal);
     } catch {
@@ -21,7 +46,7 @@ export function killProcSync(pid: number, signal?: string | number) {
 }
 
 export async function killProc(pid: number, signal?: string | number) {
-  while (procExists(pid)) {
+  while (procAlive(pid)) {
     try {
       global.process.kill(pid, signal);
     } catch {
